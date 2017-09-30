4835346e021d3fd6b33e14afd7470ba0a652c3a9
devxris <chuwei0915@gmail.com>
2017-09-30 11:10:33 +0800

    implement custom font with dynamic type by UIFontMetrics.default.scaledFont(for:)

--- CustomFonts/Controller/ViewController.swift
+++ CustomFonts/Controller/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	@IBOutlet weak var label1: UILabel!
-	{
-		didSet { label1.font = UIFont(name: "Mohave-Italic", size: 25) }
+	@IBOutlet weak var label1: UILabel! {
+		didSet {
+			// system dynamic type body font
+			label1.font = UIFont.preferredFont(forTextStyle: .body)
+			// adopt dynamic type
+			label1.adjustsFontForContentSizeCategory = true
+			label1.numberOfLines = 0
+		}
 	}
-	@IBOutlet weak var label2: UILabel!
-	{
-		didSet { label2.font = UIFont(name: "Hallo sans", size: 30) }
+	@IBOutlet weak var label2: UILabel! {
+		didSet {
+			// just implemnet custom font without dynamic type
+			label2.font = UIFont(name: "Hallo sans", size: 30)
+		}
 	}
-	@IBOutlet weak var label3: UILabel!
-	{
-		didSet { label3.font = UIFont(name: "CanterLight", size: 35) }
+	@IBOutlet weak var label3: UILabel! {
+		didSet {
+			// configure a custom font
+			let font = UIFont(name: "CanterLight", size: 35)!
+			
+			// implement UIFontMetrics default scaledFont for custom font
+			
+			// label3.font = UIFontMetrics.default.scaledFont(for: font)
+			label3.font = UIFontMetrics(forTextStyle: .title1).scaledFont(for: font)
+			
+			// adopt dynamic type for custom font
+			label3.adjustsFontForContentSizeCategory = true
+			label3.numberOfLines = 0
+		}
 	}
 }
