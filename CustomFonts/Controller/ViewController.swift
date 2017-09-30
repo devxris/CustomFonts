0de57d7c02c9ce92746121eca9d2a21487508e7c
devxris <chuwei0915@gmail.com>
2017-09-30 10:33:30 +0800

    Register customer fonts in info tab (Fonts provided by application)

--- CustomFonts/Controller/ViewController.swift
+++ CustomFonts/Controller/ViewController.swift
@@ -10,7 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
-
+	@IBOutlet weak var label1: UILabel!
+	{
+		didSet { label1.font = UIFont(name: "Mohave-Italic", size: 25) }
+	}
+	@IBOutlet weak var label2: UILabel!
+	{
+		didSet { label2.font = UIFont(name: "Hallo sans", size: 30) }
+	}
+	@IBOutlet weak var label3: UILabel!
+	{
+		didSet { label3.font = UIFont(name: "CanterLight", size: 35) }
+	}
 }
-
