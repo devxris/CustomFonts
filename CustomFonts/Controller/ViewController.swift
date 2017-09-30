ef5047c640e285224696ee486c4cf6b2a28e6d59
devxris <chuwei0915@gmail.com>
2017-09-30 09:37:10 +0800

    Simple UI and setup

---
+++ CustomFonts/Controller/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CustomFonts
+//
+//  Created by Chris Huang on 30/09/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+
+}
+
